9f0e4deba0e3a01ecd63db2f42bc7924cd4028fd
christimrichmond <christimrichmond@gmail.com>
2017-12-13 14:37:55 -0500

    final coin changer class

--- coin_changer.rb
+++ coin_changer.rb
@@ -1,26 +1,21 @@
 def get_change(cents_received)
 	coins_returned = {}
 	coins = {"quarter" => 25, "dime" => 10, "nickel" => 5, "penny" => 1}
-	
-	puts "Amount given is #{cents_received}"
-	amount_given = cents_received
-
-
-
+	end
+	puts "amount given is #{cents_received}"
+	amount_given = cents_received #copies the original value so we can change it
+	end
 coins.each do |coin, value|
 	if cents_received >= 0
-	coins_returned[coin] = cents_received / value
-	cents_received = cents_received - coin[value]
+	coins_returned[coin] = cents_received / value == 0
+	#cents_received = cents_received - coin[value]
 	end
 	puts coins_returned
-	
 end 
 
-puts "#{amount_given} amounts to #{coins_returned[quarter]} quarters 
-and #{coins_returned[dime]} dimes 
-and #{coins_returned[nickel]} nickels and #{coins_returned[penny]} pennies"
+#{coins_returned[dime]} dimes and #{coins_returned[nickel]} nickels and #{coins_returned[penny]} pennies"
 
 end
-
+end
 get_change(66)
 
